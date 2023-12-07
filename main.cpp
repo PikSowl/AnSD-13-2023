7a37ac5b75675f645c27be417280a83c275089c3
PikSowl <91498005+PikSowl@users.noreply.github.com>
2023-12-07 20:37:02 +0200

    14,15,16,17

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+/*
 #include <iostream>
 #include <fstream>
 #include <vector>
 
-using std::cout;
-using std::string;
-using std::endl;
-using std::vector;
-using std::pair;
+using cout;
+using string;
+using endl;
+using vector;
+using pair;
 
 class HashTableWithLists {
 public:
@@ -17,7 +18,7 @@ public:
     HashTableWithLists(const HashTableWithLists& table) {
         sz = table.sz;
         arr = new vector<pair<string, string>>[sz];
-        std::memcpy(arr, table.arr, sizeof(arr));
+        memcpy(arr, table.arr, sizeof(arr));
     }
     ~HashTableWithLists() {
         delete[] arr;
@@ -56,11 +57,11 @@ private:
     int sz;
 };
 
-int main() {
-    std::setlocale(LC_ALL, "RUS");
+int main(){
+    setlocale(LC_ALL, "RUS");
     cout << "lab14" << endl;
-    std::ifstream f;
-    f.open("input.txt");
+    ifstream f;
+    f.open("example.txt");
 
     if (f.is_open()) {
         int len_of_words = 0;
@@ -107,7 +108,7 @@ int main() {
         h_table["keep"] = "Hello World!";
 
 
-        std::ofstream out;
+        ofstream out;
         out.open("table.txt");
         for (int i = 0; i < h_table.size(); i++) {
             out << i << " : { ";
@@ -124,4 +125,418 @@ int main() {
 
     f.close();
 
+}
+
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+namespace BTree {
+
+    struct Node {
+        Node* left = nullptr;
+        Node* right = nullptr;
+        int value;
+        Node(int val = NULL) : value(val) {}
+    };
+
+    class BinaryTree {
+    public:
+        BinaryTree(int value = NULL);
+        BinaryTree(const BinaryTree& tree);
+        ~BinaryTree();
+
+        void init_with_string(const string& line);
+
+        void  _insert(int value);
+        Node* _search(int value);
+        void  _delete(int value);
+
+        //Обходы https://i.ytimg.com/vi/WLvU5EQVZqY/maxresdefault.jpg
+        void preorder_traversal();
+        void inorder_traversal();
+        void postorder_traversal();
+        void stack_preorder_travelsal();
+
+        void print_node(Node* node);
+        void print();
+
+    private:
+        void copy(Node* dest, Node* src);
+
+        void preorder_traversal(Node* node);
+        void inorder_traversal(Node* node);
+        void postorder_traversal(Node* node);
+        void stack_preorder_travelsal(Node* node);
+
+        Node* get_min(Node* node);
+        void  _insert(Node* node, int value);
+        Node* _search(Node* node, int value);
+        Node* _delete(Node* node, int value);
+
+        bool is_digit(char c);
+        void print_node(const std::wstring& prefix, const Node* node, bool isLeft);
+        void clear_node(Node* node);
+
+        Node* root;
+    };
+}
+
+BTree::BinaryTree::BinaryTree(int value)
+{
+    root = new Node(value);
+}
+
+BTree::BinaryTree::BinaryTree(const BinaryTree& tree)
+{
+    root = new Node();
+    copy(root, tree.root);
+}
+
+BTree::BinaryTree::~BinaryTree()
+{
+    clear_node(root);
+}
+
+void BTree::BinaryTree::_insert(int value)
+{
+    _insert(root, value);
+}
+
+BTree::Node* BTree::BinaryTree::_search(int value)
+{
+    return _search(root, value);
+}
+
+void BTree::BinaryTree::_delete(int value)
+{
+    root = _delete(root, value);
+    if (root == nullptr) root = new Node();
+    return;
+}
+
+void BTree::BinaryTree::preorder_traversal()
+{
+    preorder_traversal(root);
+}
+
+void BTree::BinaryTree::inorder_traversal()
+{
+    inorder_traversal(root);
+}
+
+void BTree::BinaryTree::postorder_traversal()
+{
+    postorder_traversal(root);
+}
+
+void BTree::BinaryTree::stack_preorder_travelsal()
+{
+    stack_preorder_travelsal(root);
+}
+
+void BTree::BinaryTree::init_with_string(const string& line)
+{
+    string number;
+    std::vector<int> numbers;
+    bool num_read = false;
+    for (char c : line) {
+        if ((c == '-') || (is_digit(c))) {
+            number += c;
+            num_read = true;
+        }
+        else {
+            if (num_read) {
+                num_read = false;
+                numbers.push_back(atoi(number.c_str()));
+                number.clear();
+            }
+
+        }
+    }
+
+    if (!number.empty()) {
+        numbers.push_back(atoi(number.c_str()));
+    }
+
+    for (auto num : numbers) {
+        _insert(root, num);
+    }
+}
+
+void BTree::BinaryTree::print_node(Node* node)
+{
+    std::wcout << L"Дерево" << endl;
+    print_node(L"", node, false);
+}
+
+void BTree::BinaryTree::print()
+{
+    print_node(root);
+}
+
+void BTree::BinaryTree::copy(Node* dest, Node* src)
+{
+    _insert(dest, src->value);
+    if (src->left != nullptr) copy(dest, src->left);
+    if (src->right != nullptr) copy(dest, src->right);
+}
+
+void BTree::BinaryTree::preorder_traversal(Node* node)
+{
+    if (node == nullptr) return;
+    cout << node->value << " ";
+    preorder_traversal(node->left);
+    preorder_traversal(node->right);
+}
+
+void BTree::BinaryTree::inorder_traversal(Node* node)
+{
+    if (node == nullptr) return;
+    inorder_traversal(node->left);
+    cout << node->value << " ";
+    inorder_traversal(node->right);
+}
+
+void BTree::BinaryTree::postorder_traversal(Node* node)
+{
+    if (node == nullptr) return;
+    postorder_traversal(node->left);
+    postorder_traversal(node->right);
+    cout << node->value << " ";
+}
+
+void BTree::BinaryTree::stack_preorder_travelsal(Node* node)
+{
+    Node* temp = node;
+    std::stack<Node*> nodes;
+    while ((temp != nullptr) || (!nodes.empty())) {
+        if (!nodes.empty()) {
+            temp = nodes.top();
+            nodes.pop();
+        }
+
+        while (temp != nullptr) {
+            cout << temp->value << " ";
+
+            if (temp->right != nullptr) {
+                nodes.push(temp->right);
+            }
+
+            temp = temp->left;
+        }
+    }
+}
+
+BTree::Node* BTree::BinaryTree::get_min(Node* node)
+{
+    if (node == nullptr) return node;
+    if (node->left == nullptr) return node;
+    return get_min(node->left);
+}
+
+void BTree::BinaryTree::_insert(Node* node, int value)
+{
+    if (node->value == NULL) {
+        node->value = value;
+        return;
+    }
+
+    if (value < node->value) {
+        if (node->left == nullptr) {
+            node->left = new Node(value);
+        }
+        else _insert(node->left, value);
+    }
+    else if (value > node->value) {
+        if (node->right == nullptr) {
+            node->right = new Node(value);
+        }
+        else _insert(node->right, value);
+    }
+    else if (value == node->value) return;
+}
+
+BTree::Node* BTree::BinaryTree::_search(Node* node, int value)
+{
+    if (node == nullptr) return node;
+
+    if (node->value == value) return node;
+
+    if (value < node->value) {
+        return _search(node->left, value);
+    }
+    else {
+        return _search(node->right, value);
+    }
+}
+
+BTree::Node* BTree::BinaryTree::_delete(Node* node, int value)
+{
+    if (node == nullptr) return node;
+
+    else if (value < node->value) {
+        node->left = _delete(node->left, value);
+    }
+    else if (value > node->value) {
+        node->right = _delete(node->right, value);
+    }
+    else {
+        if ((node->left == nullptr) || (node->right == nullptr)) {
+            if (node->left == nullptr) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            }
+            else {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+        }
+        else {
+            Node* min_right = get_min(node->right);
+            node->value = min_right->value;
+            node->right = _delete(node->right, min_right->value);
+            return node;
+        }
+    }
+
+
+}
+
+bool BTree::BinaryTree::is_digit(char c)
+{
+    return (c >= 48) && (c <= 57);
+}
+
+void BTree::BinaryTree::print_node(const std::wstring& prefix, const Node* node, bool isLeft)
+{
+    if (node != nullptr)
+    {
+        std::wcout << prefix;
+
+        std::wcout << (isLeft ? L"│L " : L"│R ");
+
+        std::cout << node->value << std::endl;
+        print_node(prefix + (isLeft ? L"│  " : L"   "), node->left, true);
+        print_node(prefix + (isLeft ? L"│  " : L"   "), node->right, false);
+    }
+}
+
+void BTree::BinaryTree::clear_node(Node* node)
+{
+    if (node->left != nullptr)   clear_node(node->left);
+    if (node->right != nullptr)  clear_node(node->right);
+    if (node != nullptr) delete node;
+}
+
+
+void write_cmds() {
+    cout << "Выберете действие" << endl;
+    cout << "[1] Добавить" << endl;
+    cout << "[2] Удалить" << endl;
+    cout << "[3] Поиск" << endl;
+    cout << "[4] Прямой обход" << endl;
+    cout << "[5] Центрированный обход" << endl;
+    cout << "[2] Концевой обход" << endl;
+    cout << "[7] Нерекурсивный прямой обход" << endl;
+    cout << "[8] Выйти" << endl;
+}
+
+int main()
+{
+    std::setlocale(LC_ALL, "RUS");
+    BTree::BinaryTree tree;
+    tree.init_with_string("8 (3 (1, 6(4,7)), 10 (, 14(13,)))");
+
+    tree.print();
+    write_cmds();
+
+    string a;
+    cin >> a;
+
+    while (true) {
+        int num;
+        switch (a[0])
+        {
+        case '1':
+            system("cls");
+            tree.print();
+            cout << "[1] Добавить" << endl;
+            cout << "Введите значение: ";
+            cin >> num;
+            tree._insert(num);
+            system("cls");
+            break;
+        case '2':
+            system("cls");
+            tree.print();
+            cout << "[2] Удалить" << endl;
+            cout << "Введите значение: ";
+            cin >> num;
+            tree._delete(num);
+            system("cls");
+            break;
+        case '3': {
+            system("cls");
+            cout << "[3] Поиск" << endl;
+            cout << "Введите значение: ";
+            cin >> num;
+            BTree::Node* sub_tree = tree._search(num);
+
+            if (sub_tree == nullptr) {
+                cout << "Результат: Не найдено" << endl;
+            }
+            else {
+                cout << "Результат:" << endl;
+                tree.print_node(sub_tree);
+            }
+            cout << "\n\n";
+            break;
+        }
+        case '4':
+            system("cls");
+            cout << "[4] Прямой обход: ";
+            tree.preorder_traversal();
+            cout << "\n\n";
+            break;
+        case '5':
+            system("cls");
+            cout << "[5] Центрированный обход: ";
+            tree.inorder_traversal();
+            cout << "\n\n";
+            break;
+        case '6':
+            system("cls");
+            cout << "[6]Концевой обход: ";
+            tree.postorder_traversal();
+            cout << "\n\n";
+            break;
+        case '7':
+            system("cls");
+            cout << "[7]Нерекурсивный прямой обход: ";
+            tree.stack_preorder_travelsal();
+            cout << "\n\n";
+            break;
+        case '8':
+            system("cls");
+            return 0;
+        default:
+            system("cls");
+            break;
+        }
+        tree.print();
+        write_cmds();
+        cin >> a;
+    }
+    return 0;
 }
